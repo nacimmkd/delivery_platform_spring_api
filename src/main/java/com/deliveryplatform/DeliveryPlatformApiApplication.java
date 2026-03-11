84a2057024fc661173c1b9365f109970a708cce7
nacimmkd <nacimmakedhi@gmail.com>
2026-03-11 10:38:18 +0100

    init project

---
+++ src/main/java/com/deliveryplatform/DeliveryPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryplatform/DeliveryPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
